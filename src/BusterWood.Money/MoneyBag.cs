389bac9cf024a6021fb67cceab7f0e9c6a426805
busterwood <chris.j.austin@gmail.com>
2017-03-31 13:09:41 +0100

    Add documentation to MoneyBag

--- src/BusterWood.Money/MoneyBag.cs
+++ src/BusterWood.Money/MoneyBag.cs
@@ -4,13 +4,14 @@ using System.Text;
 
 namespace BusterWood.Monies
 {
-    /// <summary>
-    /// A container for money of one or more currencies, i.e. 10 GBP and 20 USD
-    /// </summary>
+    /// <summary>A container for money of one or more currencies, i.e. 10 GBP and 20 USD</summary>
     public class MoneyBag : IEnumerable<Money>
     {
         readonly Dictionary<string, decimal> _contents = new Dictionary<string, decimal>();
 
+        /// <summary>Gets the amount of money in the bag of a specific currency</summary>
+        /// <param name="currency">ISO Code of the currency, e.g. GBP, USD, EUR</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown if currency is not an ISO Code</exception>
         public Money this[string currency]
         {
             get
@@ -22,6 +23,7 @@ namespace BusterWood.Monies
             }
         }
 
+        /// <summary>Increases the amount of a currency in the bag</summary>
         public void Add(Money money)
         {
             Money.CheckIsoCode(money.Currency);
@@ -30,8 +32,11 @@ namespace BusterWood.Monies
             _contents[money.Currency] = amount + money.Amount;
         }
 
+        /// <summary>Has any <see cref="Money"/> been added to this bag, or substracted from it?</summary>
+        /// <param name="currency">ISO Code of the currency, e.g. GBP, USD, EUR</param>
         public bool Contains(string currency) => _contents.ContainsKey(currency);
 
+        /// <summary>Enumerates the total values of all currencies in this bag</summary>
         public IEnumerator<Money> GetEnumerator()
         {
             foreach (var pair in _contents)
@@ -42,6 +47,7 @@ namespace BusterWood.Monies
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>Does the bag contain a non-zero amount of any currency?</summary>
         public bool IsEmpty
         {
             get
@@ -56,6 +62,9 @@ namespace BusterWood.Monies
             }
         }
 
+        /// <summary>Removes all the money of a specific <paramref name="currency"/> from this bag.</summary>
+        /// <param name="currency">The currency to remove</param>
+        /// <returns>The amount of <see cref="Money"/> removed</returns>
         public Money Remove(string currency)
         {
             Money.CheckIsoCode(currency);
@@ -68,8 +77,10 @@ namespace BusterWood.Monies
             return new Money(0m, currency);
         }
 
+        /// <summary>Reduces the amount of a currency in the bag</summary>
         public void Subtract(Money money) => Add(-money);
 
+        /// <summary>Shows what currency is in the bag</summary>
         public override string ToString()
         {
             var sb = new StringBuilder(10);
